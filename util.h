03a845fbe05fdf4074d83648e2ce3c6ea93ba29b
Tamarozi <cauaptamarozi@gmail.com>
2025-04-16 20:05:03 -0300

    Add files via upload

---
+++ util.h
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+double diametro (double raio) {
+    return raio * 2;
+}
+
+double circunferencia (double raio) {
+    return 2 * 3.14 * raio;
+}
+
+double area (double raio) {
+    return 3.14 * raio * raio;
+} 
